346701789f0e98f5c8772ed161b43bd343c1724c
Mirco Bochicchio <69087218+1boch1@users.noreply.github.com>
2024-03-30 02:23:27 +0100

    Commented

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 const Leap = require('leapjs');
 const { SerialPort } = require('serialport')
 
-// available ports' paths
-SerialPort.list().then(ports => {
-  ports.forEach(function (port) {
+// available ports
+SerialPort.list().then(ports =>
+{
+  ports.forEach(function (port)
+  {
     console.log(port.path);
   })
 })
 
 let port = new SerialPort({
-  path: 'COM4',
+  path: 'COM4',  // find the path of your port and put it here
   baudRate: 115200,
 });
 
 // open errors will be emitted as an error event
-port.on('error', function (err) {
+port.on('error', function (err)
+{
   console.log('Error: ', err.message);
 
+  // alternative path
   port = new SerialPort({
     path: 'COM3',
     baudRate: 115200,
@@ -25,55 +29,63 @@ port.on('error', function (err) {
 
 var controller = new Leap.Controller()
 
-controller.on('ready', function () {
+controller.on('ready', function ()
+{
   console.log("ready");
 });
-controller.on('connect', function () {
+controller.on('connect', function ()
+{
   console.log("connect");
 });
-controller.on('disconnect', function () {
+controller.on('disconnect', function ()
+{
   console.log("disconnect");
 });
-controller.on('focus', function () {
+controller.on('focus', function ()
+{
   console.log("focus");
 });
-controller.on('blur', function () {
+controller.on('blur', function ()
+{
   console.log("blur");
 });
-controller.on('deviceConnected', function () {
+controller.on('deviceConnected', function ()
+{
   console.log("deviceConnected");
 });
-controller.on('deviceDisconnected', function () {
+controller.on('deviceDisconnected', function ()
+{
   console.log("deviceDisconnected");
 });
 
 controller.connect();
 console.log("\nWaiting for device to connect...");
 
-//////////////////////
-// Robot arm controls
-/////////////////////
+/////////////////////////
+// Robot arm controls //
+////////////////////////
 
 var pos = null; // position of the hand in the current frame
 var lastPos = null; // position of the hand in the last frame
 
-var start = false;
+var start = false; // start tracking
 
-var thr = 0.0004;
+var thr = 0.0004; // threshold to ingore micro movements
 
-Leap.loop(function (frame) {
+Leap.loop(function (frame)
+{
+  if (frame.hands?.length <= 0) return; // if there are no hands in the scene
 
-  if (frame?.hands?.length <= 0) return;
-
-  pos = frame.hands[0]?.palmPosition;
+  pos = frame.hands[0]?.palmPosition; // get the position of the hand (the first hand)
   if (!pos) return;
 
-  let n_fingers = frame?.fingers?.filter(f => f.extended).length;
+  let n_fingers = frame?.fingers?.filter(f => f.extended).length; // get the number of extended fingers
 
-  // show 1 finger to start controlling the robot arm
-  if (n_fingers == 3) {
+  // show 3 fingers to start controlling the robot arm
+  if (n_fingers == 3)
+  {
     start = true;
-    lastPos = pos;
+    lastPos = pos; // setup lastPos
   }
 
   if (!start) return;
@@ -81,21 +93,24 @@ Leap.loop(function (frame) {
   let [z, x, y] = pos;
   let [lz, lx, ly] = lastPos;
 
-  let moveX = (lx - x)/4000;
+  let moveX = (lx - x) / 4000;
 
-  if(Math.abs(moveX) >= thr) port.write('G20G91G1X' + moveX + 'F250\n\r', (err) => {
-    if (err) console.log('Error on write: ', err.message) 
+  if (Math.abs(moveX) >= thr) port.write('G20G91G1X' + moveX + 'F250\n\r', (err) =>
+  {
+    if (err) console.log('Error on write: ', err.message)
   });
 
-  let moveY = (ly - y)/3000;
+  let moveY = (ly - y) / 3000;
 
-  if(Math.abs(moveY) >= thr) port.write('G20G91G1Y' + moveY + 'F250\n\r', (err) => {
+  if (Math.abs(moveY) >= thr) port.write('G20G91G1Y' + moveY + 'F250\n\r', (err) =>
+  {
     if (err) return console.log('Error on write: ', err.message)
   });
 
-  let moveZ = (lz - z)/2000;
+  let moveZ = (lz - z) / 2000;
 
-  if(Math.abs(moveZ) >= thr) port.write('G20G91G1Z' + moveZ + 'F250\n\r', function (err) {
+  if (Math.abs(moveZ) >= thr) port.write('G20G91G1Z' + moveZ + 'F250\n\r', function (err)
+  {
     if (err) return console.log('Error on write: ', err.message)
   });
 
